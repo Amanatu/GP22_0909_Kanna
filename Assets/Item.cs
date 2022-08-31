e88b419c1b586fb65a13edc595d61be637239b3a
Kanna <amanatukanna@gmail.com>
2022-08-31 18:09:58 +0900

    プレハブの生成

---
+++ Assets/Item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    Rigidbody2D item_rb;
+    [SerializeField]
+    public float hight = 5; //çÇÇ≥
+    [SerializeField]
+    public float wide = 10; //â°
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Item_Sp.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Sp : MonoBehaviour
+{
+    //生成と打ち出しをスポナーでやる
+    [SerializeField]
+    Item[] items;
+
+    [SerializeField]
+    Vector2 force_max = new Vector2(15.0f, 10.0f);
+
+    //時間
+    [SerializeField]
+    float item_sp_time_max = 3.0f;
+    float item_sp_time;
+    float item_sp_timer = 0;
+
+    void Start()
+    {
+        item_sp_timer = item_sp_time_max;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        item_sp_timer -= Time.deltaTime;
+        if(item_sp_timer <= 0)
+        {
+            Item normal = Instantiate(items[0]);
+            normal.name = "normal_item";
+            var wide = Random.Range(-force_max.x, force_max.x);
+            normal.GetComponent<Rigidbody2D>().AddForce(new Vector2(force_max.x, force_max.y), ForceMode2D.Impulse);
+            //時間ランダム
+            item_sp_timer = Random.Range(0.1f,item_sp_time_max);
+        }
+
+    }
+}
